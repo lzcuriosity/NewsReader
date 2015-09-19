b68c20e3a47ef9c2e984710680afc77c6961b377
lzcuriosity <476194489@qq.com>
2015-09-20 01:46:34 +0800

    补充

---
+++ NewsReader/NewsReader/Controller/Page/HomePage.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsReader
+//
+//  Created by lz on 15/9/19.
+//
+//
+
+#import "FxBasePage.h"
+
+@interface HomePage : FxBasePage
+
+@end
+
+  
---
+++ NewsReader/NewsReader/Controller/Page/HomePage.m
@@ -0,0 +1,14 @@
+//
+//  ViewController.m
+//  NewsReader
+//
+//  Created by lz on 15/9/19.
+//
+//
+
+#import "HomePage.h"
+
+@implementation HomePage
+
+
+@end
